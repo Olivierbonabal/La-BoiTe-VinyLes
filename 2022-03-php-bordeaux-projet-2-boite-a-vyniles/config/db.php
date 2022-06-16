429031988edf7df9ed7236992da4cfd073b71227
Olivierbonabal <100704099+Olivierbonabal@users.noreply.github.com>
2022-06-16 17:13:36 +0200

    Add files via upload

---
+++ 2022-03-php-bordeaux-projet-2-boite-a-vyniles/config/db.php
@@ -0,0 +1,6 @@
+<?Php
+
+define('APP_DB_HOST', 'localhost');
+define('APP_DB_NAME', 'project2');
+define('APP_DB_USER', 'root');
+define('APP_DB_PASSWORD', '1234');
---
+++ 2022-03-php-bordeaux-projet-2-boite-a-vyniles/public/assets/JS/top.js
@@ -0,0 +1,93 @@
+function moveToSelected(element) {
+
+    if (element == "next") {
+        var selected = $(".selected").next();
+    } else if (element == "prev") {
+        var selected = $(".selected").prev();
+    } else {
+        var selected = element;
+    }
+
+    var next = $(selected).next();
+    var prev = $(selected).prev();
+    var prevSecond = $(prev).prev();
+    var nextSecond = $(next).next();
+
+    $(selected).removeClass().addClass("selected");
+
+    $(prev).removeClass().addClass("prev");
+    $(next).removeClass().addClass("next");
+
+    $(nextSecond).removeClass().addClass("nextRightSecond");
+    $(prevSecond).removeClass().addClass("prevLeftSecond");
+
+    $(nextSecond).nextAll().removeClass().addClass('hideRight');
+    $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+
+}
+
+// Eventos teclado
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+            moveToSelected('prev');
+            break;
+
+        case 39: // right
+            moveToSelected('next');
+            break;
+
+        default: return;
+    }
+    e.preventDefault();
+});
+
+$('#carousel div').click(function() {
+    moveToSelected($(this));
+});
+
+$('#prev').click(function() {
+    moveToSelected('prev');
+});
+
+$('#next').click(function() {
+    moveToSelected('next');
+});
+
+
+
+// $('.responsive').slick({
+//     dots: true,
+//     infinite: false,
+//     speed: 300,
+//     slidesToShow: 4,
+//     slidesToScroll: 4,
+//     responsive: [
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 3,
+//                 infinite: true,
+//                 dots: true
+//             }
+//         },
+//         {
+//             breakpoint: 600,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 2
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1
+//             }
+//         }
+//         // You can unslick at a given breakpoint now by adding:
+//         // settings: "unslick"
+//         // instead of a settings object
+//     ]
+// });
